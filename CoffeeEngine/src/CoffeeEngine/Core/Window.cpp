c7071c15e7a73952868d3159e23d4ba2c78b3b3c
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-09-29 12:51:17 +0200

    remove unused variables

--- CoffeeEngine/src/CoffeeEngine/Core/Window.cpp
+++ CoffeeEngine/src/CoffeeEngine/Core/Window.cpp
@@ -48,8 +48,6 @@ namespace Coffee {
 			COFFEE_CORE_ASSERT(success, "Could not initialize SDL!");
 		}
 
-            const char* glsl_version = "#version 450";
-
             SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
             SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
             SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
