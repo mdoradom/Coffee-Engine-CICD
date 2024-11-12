33fd0e6a7d51abd2b6227ea9c521de84380ad9e9
mdoradom <mariodoradomartinez@gmail.com>
2024-11-12 12:39:40 +0100

    added uuid class

---
+++ CoffeeEngine/src/CoffeeEngine/Core/UUID.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstddef>
+#include <glm/fwd.hpp>
+
+namespace Coffee {
+
+    class UUID
+    {
+    public:
+        UUID();
+        UUID(uint64_t uuid);
+        UUID(const UUID&) = default;
+
+        operator uint64_t() const { return m_UUID; }
+    private:
+        uint64_t m_UUID;
+    };
+
+}
+
+namespace std {
+    template <typename T> struct hash;
+
+    template<>
+    struct hash<Coffee::UUID>
+    {
+        std::size_t operator()(const Coffee::UUID& uuid) const
+        {
+            return (glm::uint64_t)uuid;
+        }
+    };
+
+}
