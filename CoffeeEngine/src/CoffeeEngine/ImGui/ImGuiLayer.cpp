03de7d5ba6f58f513213c65d0e5189121a0b4f55
mdoradom <mariodoradomartinez@gmail.com>
2024-11-12 11:13:44 +0100

    removed dpi code from this branch

--- CoffeeEngine/src/CoffeeEngine/ImGui/ImGuiLayer.cpp
+++ CoffeeEngine/src/CoffeeEngine/ImGui/ImGuiLayer.cpp
@@ -40,22 +40,9 @@ namespace Coffee {
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
         //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable; // Comment this to disable the detached imgui windows from the main window
 
-
-        // ================ This is temporal please Hugo do it correctly ===============//
-        Application& app = Application::Get();
-        SDL_Window* window = static_cast<SDL_Window*>(app.GetWindow().GetNativeWindow());
-
-        COFFEE_ERROR("DPI Scaling: {0}", SDL_GetWindowDisplayScale(window));
-
-        float dpiScaling = SDL_GetWindowDisplayScale(window);
-
-        ImGui::GetStyle().ScaleAllSizes(dpiScaling);
-
-        float fontSize = 17.5f * dpiScaling;
+        float fontSize = 17.5f;
         float iconFontSize = fontSize;
         io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/JetBrains_Mono/static/JetBrainsMono-Medium.ttf", fontSize);
-
-        // ================ This is temporal please Hugo do it correctly (END) ===============//
 
         // Load icon font
         static const ImWchar icon_ranges[] = { ICON_MIN_LC, ICON_MAX_LC, 0 }; // Adjust this range according to your icons
@@ -67,6 +54,9 @@ namespace Coffee {
         io.Fonts->AddFontFromFileTTF("assets/fonts/lucide.ttf", iconFontSize, &icon_config, icon_ranges); // FIXME the size of the font makes the icons beeing not centered https://github.com/ocornut/imgui/blob/master/docs/FONTS.md
 
         SetCoffeeColorStyle();
+
+        Application& app = Application::Get();
+        SDL_Window* window = static_cast<SDL_Window*>(app.GetWindow().GetNativeWindow());
 
         ImGui_ImplSDL3_InitForOpenGL(window, SDL_GL_GetCurrentContext());
         ImGui_ImplOpenGL3_Init("#version 410");
