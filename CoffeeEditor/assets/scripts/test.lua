0663c1ac984d49503d85e4c82158d597ed13f580
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-11-29 11:29:19 +0100

    some glue code

--- CoffeeEditor/assets/scripts/test.lua
+++ CoffeeEditor/assets/scripts/test.lua
@@ -1,11 +1,18 @@
 function OnCreate()
     print("OnCreate()")
+    log("OnCreate()")
+    log_error("OnCreate()")
+    log_warning("OnCreate()")
+    log_critical("OnCreate()")
 end
 
 function OnUpdate()
-    print("OnUpdate()")
+    -- print("OnUpdate()")
+    if is_key_pressed(SPACE) then
+        log("SPACE")
+    end
 end
 
-function OnDestroy()
-    print("OnDestroy()")
+function on_destroy()
+    -- print("OnDestroy()")
 end
