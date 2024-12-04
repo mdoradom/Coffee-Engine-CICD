89d48cb5b5044606039c27e4b25661ff3517a6bf
Hugo Planell <69766683+HugoPlacer@users.noreply.github.com>
2024-12-04 22:55:28 +0100

    SimpleGridShader Update

--- CoffeeEditor/assets/shaders/SimpleGridShader.glsl
+++ CoffeeEditor/assets/shaders/SimpleGridShader.glsl
@@ -30,14 +30,23 @@ in vec2 TexCoord;
 void main()
 {
     float lineWidth = 0.02;
-    float gridSize = 1000;
+    float gridSize = 1000.0;
 
     vec2 uv = TexCoord * gridSize;
 
-    vec2 lineAA = fwidth(uv);
-    vec2 gridUV = 1.0 - abs(fract(uv) * 2.0 - 1.0);
-    vec2 grid2 = smoothstep(lineWidth + lineAA, lineWidth - lineAA, gridUV);
-    float grid = grid2.x * (1.0 - grid2.y) + grid2.y;
+    vec4 uvDDXY = vec4(dFdx(uv), dFdy(uv));
+    vec2 uvDeriv = vec2(length(uvDDXY.xz), length(uvDDXY.yw));
+    bvec2 invertLine = greaterThan(vec2(lineWidth), vec2(0.5));
+    vec2 targetWidth = mix(vec2(lineWidth), vec2(1.0 - lineWidth), invertLine);
+    vec2 drawWidth = clamp(targetWidth, uvDeriv, vec2(0.5));
+    vec2 lineAA = uvDeriv * 1.5;
+    vec2 gridUV = abs(fract(uv) * 2.0 - 1.0);
+    gridUV = mix(1.0 - gridUV, gridUV, invertLine);
+    vec2 grid2 = smoothstep(drawWidth + lineAA, drawWidth - lineAA, gridUV);
+    grid2 *= clamp(targetWidth / drawWidth, vec2(0.0), vec2(1.0));
+    grid2 = mix(grid2, targetWidth, clamp(uvDeriv * 2.0 - 1.0, vec2(0.0), vec2(1.0)));
+    grid2 = mix(grid2, 1.0 - grid2, invertLine);
+    float grid = mix(grid2.x, 1.0, grid2.y);
 
-    FragColor = vec4(grid * 0.75);
+    FragColor = vec4(grid);
 }
